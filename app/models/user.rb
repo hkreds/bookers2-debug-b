7f1fa590a5dd88e7c1e6325c9896f400c6adea26
hkreds <109258421+hkreds@users.noreply.github.com>
2022-08-22 04:04:59 +0000

    [Add]relationshipsコントローラーと_infoのview

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  has_many :follower, class_name: "Relationship", foreign_key: "@user_id", dependent: :destroy
-  has_many :followed, class_name: "Relationship", foreign_key: "@user_id", dependent: :destroy
-  has_many :followings, through: :follower, source: :followed
-  has_many :followers, through: :followed, source: :follower
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   def follow(user_id)
     follower.create(followed_id: user_id)
   end
@@ -34,5 +34,5 @@ class User < ApplicationRecord
   def following?(user)
     following_user.include?(user)
   end
-  
+
 end
