a1bbc740de1140dbe3a4a2c9d632213d0a148b74
hkreds <109258421+hkreds@users.noreply.github.com>
2022-08-22 01:32:49 +0000

    [Add]モデル記述とルーティング

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,30 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  has_many :follower, class_name: "Relationship", foreign_key: "@user_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "@user_id", dependent: :destroy
+  has_many :followings, through: :follower, source: :followed
+  has_many :followers, through: :followed, source: :follower
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
+  
 end
